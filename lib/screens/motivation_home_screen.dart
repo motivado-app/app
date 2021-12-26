b4ebee00152584fd1f94feb889043508efcc04ec
Advaita Saha <adv11sphs@gmail.com>
2021-12-26 14:05:53 +0530

    comments removed, code cleaned

--- lib/screens/motivation_home_screen.dart
+++ lib/screens/motivation_home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
